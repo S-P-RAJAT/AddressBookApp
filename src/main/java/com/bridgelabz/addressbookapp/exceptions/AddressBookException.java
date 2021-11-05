d9cd0fb42cd7d9a6c06847955b64d46dbdc3069b
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 03:22:11 +0530

    ADDED: Custom exception class for address book exceptions

---
+++ src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookException.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbookapp.exceptions;
+
+public class AddressBookException extends RuntimeException
+{
+    public AddressBookException(String message)
+    {
+        super(message);
+    }
+    
+}
