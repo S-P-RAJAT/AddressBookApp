64eefbe9e7811121602bf0361cf34f29ef7bf258
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 01:16:07 +0530

    ADDED: Source files

---
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
