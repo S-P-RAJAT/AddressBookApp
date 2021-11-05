f2bb5d16d4b7904033521a50a74fffe1e80781dc
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 02:37:27 +0530

    ADDED: Use slf4j from lombok library for logging

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -3,11 +3,14 @@ package com.bridgelabz.addressbookapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Employee payroll app started");
 	}
-
 }
