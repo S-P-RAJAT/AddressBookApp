1f468ac74a67182f899e63faf31464db3f10d53f
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 01:38:07 +0530

    ADDED: Rest call to get list of contacts and their details

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.addressbookapp.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,34 +19,10 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 @RequestMapping("/addressbookservice")
 public class AddressBookController {
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-
 	@RequestMapping(value = { "", "/", "/get" })
-	public String addressBook() {
-		return String.format(template, "World");
-	}
+	public ResponseEntity<String> getContactData() {
 
-	@GetMapping("/query")
-	public String addressBook(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format(template, name);
-	}
+		return new ResponseEntity<String>("Get call success", HttpStatus.OK);
 
-
-	@GetMapping("/param/{name}")
-	public String addressBookParam(@PathVariable String name) {
-		return String.format(template, name);
-	}
-
-	@PostMapping("/post")
-	public String addressBook(@RequestBody ContactDTO contact) {
-
-		return String.format(template, contact.getFirstName() + " " + contact.getLastName());
-	}
-
-	@PutMapping("put/{firstName}")
-	public String addressBook(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
-
-		return String.format(template, firstName + " " + lastName);
 	}
 }
