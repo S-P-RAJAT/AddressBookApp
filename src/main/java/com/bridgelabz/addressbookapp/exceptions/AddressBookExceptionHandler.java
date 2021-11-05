7a43c4257ce6ae718bf15501dc0ebe2f4832fc08
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 03:22:37 +0530

    ADDED: User friendly exception for contact not found

--- src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionHandler.java
+++ src/main/java/com/bridgelabz/addressbookapp/exceptions/AddressBookExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.bridgelabz.addressbookapp.exceptions;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.bridgelabz.addressbookapp.dto.ResponseDTO;
-
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,20 +12,23 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 @ControllerAdvice
-public class AddressBookExceptionHandler 
-{
+public class AddressBookExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public  ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception)
-    {
-        List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
-        List<String> errMesg=errorList.stream()
-                             .map(objerr->objerr.getDefaultMessage())
-                             .collect(Collectors.toList());
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+			MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMesg = errorList.stream().map(objerr -> objerr.getDefaultMessage())
+				.collect(Collectors.toList());
 
-        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errMesg);
-        return new ResponseEntity<ResponseDTO>(responseDTO,HttpStatus.BAD_REQUEST);
-    }
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errMesg);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(AddressBookException.class)
+	public ResponseEntity<ResponseDTO> handleEmployeePayrollException(AddressBookException exception) {
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", exception.getMessage());
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
 }
