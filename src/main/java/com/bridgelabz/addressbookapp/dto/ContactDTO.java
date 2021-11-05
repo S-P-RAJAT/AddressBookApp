a8e03150a40a5db63299351706b6ec363fc63ede
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 03:27:45 +0530

    ADDED: Remaining attributes of contact class

--- src/main/java/com/bridgelabz/addressbookapp/dto/ContactDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ContactDTO.java
@@ -4,27 +4,40 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public class ContactDTO {
-	@NotEmpty(message = "Contact name cannot be null")
-	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "First Name Invalid")
-	public String firstName;
 
-	@NotEmpty(message = "Contact name cannot be null")
-	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$", message = "Last Name Invalid")
-	public String lastName;
-
+	@NotEmpty(message = "Name cannot be null")
+	@Pattern(regexp = "^[A-Z]{1}[a-z]{2,}$",message = "Name Invalid")
+    public String name;
+	
+	@NotEmpty(message = "Address cannot be null")
 	@Pattern(regexp = "^[A-Z,a-z,0-9, ()#-]{3,}$", message = "Address Invalid")
-	public String address;
+    public String address;
 
-	public ContactDTO(String firstName, String lastName, String address) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
+	@NotEmpty(message = "phone number cannot be null")
+	@Pattern(regexp = "^[+]?[91]{2}[ ]?[6-9][0-9]{9}$",message = "Phone number Invalid")
+	public String phoneNumber;
+
+	public String city;
+	public String state;
+
+	@NotEmpty(message = "zip cannot be null")
+	@Pattern(regexp = "[0-9]{3}[ ]?[0-9]{3}$",message = "Zip Invalid")
+	public String zip;
+
+	public ContactDTO( String name, String address, String phoneNumber, String city,String state, String zip) 
+	{
+		this.name = name;
 		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
 	}
 
 	@Override
 	public String toString() 
 	{
-		return "ContactDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + "]";
+		return "ContactDTO [name=" + name + ", address=" + address + ", phoneNumber=" + phoneNumber + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + "]";
 	}
 }
