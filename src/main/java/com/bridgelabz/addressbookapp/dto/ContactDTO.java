784f2546ff3a2c006120854176b47c42a4baa02b
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 01:32:22 +0530

    ADDED: Contact DTO

---
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ContactDTO.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.addressbookapp.dto;
+
+public class ContactDTO 
+{
+    public String firstName;
+    public String lastName;
+    public String address;
+    
+	public ContactDTO(String firstName, String lastName, String address) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
