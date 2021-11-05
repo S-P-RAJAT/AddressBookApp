d9585972e7a2c2192146b666197f1ea04ed52e0d
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-06 02:31:57 +0530

    REFACTORED: Use lombok library to generate getters & setters for DTO

--- src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.addressbookapp.dto;
 
-public class ResponseDTO {
+import lombok.Data;
+
+public @Data class ResponseDTO {
 	private String message;
 	private Object data;
 
@@ -9,19 +11,4 @@ public class ResponseDTO {
 		this.data = data;
 	}
 
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
 }
